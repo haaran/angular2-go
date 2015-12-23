7f99c714dfe471499d7bb735732f270ba487ede1
Hariskumar Panakkal <hariskumar@gmail.com>
2015-12-23 11:06:15 -0600

    added todos

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,19 +2,21 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {CharactersComponent} from './characters.component';
 import {DashboardComponent} from './dashboard.component';
+import {TodosComponent} from './todos.component';
 
 @Component({
   selector: 'my-app',
   template: `
     <a [routerLink]="['Dashboard']">Dashboard</a>
     <a [routerLink]="['Characters']">Characters</a>
+    <a [routerLink]="['Todos']">Todos</a>
     <router-outlet></router-outlet>
     `,
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  // { path: '/', redirectTo: ['Dashboard'] },
   { path: '/dashboard', as: 'Dashboard', component: DashboardComponent, useAsDefault: true },
-  { path: '/characters', as: 'Characters', component: CharactersComponent }
+  { path: '/characters', as: 'Characters', component: CharactersComponent },
+  { path: '/todos', as: 'Todos', component: TodosComponent }
 ])
 export class AppComponent { }
--- app/todos.component.ts
+++ app/todos.component.ts
@@ -1,16 +1,16 @@
 import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'my-dashboard',
+  selector: 'my-todos',
 	template: `
-		<h2>Dashboard</h2>
+		<h2>Todos</h2>
 		<div>Hello {{name}}</div>
 		<input [(ngModel)]="name" />
 		<button (click)="sayHello()">Say Hello</button>
 		<p>{{message}}</p>
 	`
 })
-export class DashboardComponent {
+export class TodosComponent {
 	public name = 'Hari';
 	public message = '';
 
